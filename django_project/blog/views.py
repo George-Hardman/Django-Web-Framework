a76853c95bd4428301d8e69aadeddb1ea785bf6e
George Hardman <georgehardman96@gmail.com>
2020-01-30 14:58:03 +0000

    Created an html template and used template inheritance

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
+posts = [
+    {
+        'author': 'GeorgeHardman',
+        'title': 'Blog post 1',
+        'content': 'First Post Content',
+        'date_posted': 'January 29, 2020'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog post 2',
+        'content': 'Second Post Content',
+        'date_posted': 'January 30, 2020'
+    },
+]
 
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse("<h1>About<h1>")
+    return render(request, 'blog/about.html', {'title': 'About'})
