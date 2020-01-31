0a38a8bb1e40f6ac6976f86376a36b407321b549
George Hardman <georgehardman96@gmail.com>
2020-01-31 21:08:21 +0000

    Created Post models and changed date on interface

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-
-
-posts = [
-    {
-        'author': 'GeorgeHardman',
-        'title': 'Blog post 1',
-        'content': 'First Post Content',
-        'date_posted': 'January 29, 2020'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog post 2',
-        'content': 'Second Post Content',
-        'date_posted': 'January 30, 2020'
-    },
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
